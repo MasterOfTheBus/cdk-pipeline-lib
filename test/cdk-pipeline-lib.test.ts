aff908fa3ad2e213b7116b628ebf1669e3625414
Sidney Ng <yendisng@gmail.com>
2021-12-04 15:27:50 -0500

    adding multi source pipeline test

--- test/cdk-pipeline-lib.test.ts
+++ test/cdk-pipeline-lib.test.ts
@@ -8,15 +8,11 @@ test('Pipeline Single Source', () => {
     const app = new cdk.App();
     const stack = new cdk.Stack(app, 'TestStack');
 
-    const codeStarArn = "arn:aws:codestar-connections:us-east-1:000000000000:connection/11111111-2222-3333-4444-555555555555";
-    const repo = "test-repo";
-    const repoOwner = "test-owner";
-    const branch = "main";
     const source = new CodeStarConnectionDef({
-        codeStarConnection: codeStarArn,
-        repo: repo,
-        repoOwner: repoOwner,
-        branch: branch
+        codeStarConnection: "arn:aws:codestar-connections:us-east-1:000000000000:connection/11111111-2222-3333-4444-555555555555",
+        repo: "test-repo",
+        repoOwner: "test-owner",
+        branch: "main"
     });
 
     // Define the bucket to store the artifacts
@@ -47,28 +43,85 @@ test('Pipeline Single Source', () => {
     const buildStage = buildStageCapture.asObject();
 
     // Source Actions
-    checkSourceStageValues(sourceStage, source);
+    checkSourceStageValues(sourceStage, [source]);
     
     // Build Actions
     checkBuildStageValues(buildStage, source, projectRef);
 });
 
-test('Pipeline Multiple Sources', () => {});
+test('Pipeline Multiple Sources', () => {
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, 'TestStack');
+
+    const source = new CodeStarConnectionDef({
+        codeStarConnection: "arn:aws:codestar-connections:us-east-1:000000000000:connection/11111111-2222-3333-4444-555555555555",
+        repo: "test-repo",
+        repoOwner: "test-owner",
+        branch: "main"
+    });
+
+    const source2 = new CodeStarConnectionDef({
+        codeStarConnection: "arn:aws:codestar-connections:us-east-1:000000000000:connection/11111111-2222-3333-4444-555555555555",
+        repo: "test-repo-2",
+        repoOwner: "test-owner",
+        branch: "release"
+    });
+
+    // Define the bucket to store the artifacts
+    const bucket = new Bucket(stack, 'PipelineBucket');
+
+    new MultiSourcePipeline(stack, 'MultiSourcePipline', {
+        sources: [source, source2],
+        deployBucket: bucket
+    });
+
+    const template = Template.fromStack(stack);
+
+    const sourceStageCapture = new Capture();
+    const buildStageCapture = new Capture();
+    const build2StageCapture = new Capture();
+
+    // Just check that the project is created, but don't need to test anything
+    template.resourceCountIs('AWS::CodeBuild::Project', 2);
+    const projectResources = template.findResources('AWS::CodeBuild::Project');
+    const projectRef = Object.keys(projectResources)[0];
+    const project2Ref = Object.keys(projectResources)[1];
+
+    template.resourceCountIs('AWS::CodePipeline::Pipeline', 1);
+    template.hasResourceProperties('AWS::CodePipeline::Pipeline', {
+        Stages: [sourceStageCapture, buildStageCapture, build2StageCapture]
+    });
+
+    // There are 2 sources, so 1 source stage and 2 build stages
+    const sourceStage = sourceStageCapture.asObject();
+    const buildStage = buildStageCapture.asObject();
+    const build2Stage = build2StageCapture.asObject();
+
+    // Source Actions
+    checkSourceStageValues(sourceStage, [source, source2]);
+    
+    // Build Actions
+    checkBuildStageValues(buildStage, source, projectRef);
+    checkBuildStageValues(build2Stage, source2, project2Ref);
+});
 
 // TODO: Support other source types
-const checkSourceStageValues = (sourceStage: any, sourceDef: CodeStarConnectionDef) => {
+const checkSourceStageValues = (sourceStage: any, sourceDefs: CodeStarConnectionDef[]) => {
     expect(sourceStage.Name).toBe('Source');
     expect(sourceStage.Actions).toBeTruthy();
-    expect(sourceStage.Actions.length).toEqual(1);
+    expect(sourceStage.Actions.length).toEqual(sourceDefs.length);
     
-    const sourceAction = sourceStage.Actions[0];
-    expect(sourceAction.ActionTypeId.Category).toEqual('Source');
-    expect(sourceAction.ActionTypeId.Provider).toEqual('CodeStarSourceConnection');
-    expect(sourceAction.Configuration.ConnectionArn).toEqual(sourceDef.codeStarConnection);
-    expect(sourceAction.Configuration.FullRepositoryId).toEqual(`${sourceDef.repoOwner}/${sourceDef.repo}`);
-    expect(sourceAction.Configuration.BranchName).toEqual(sourceDef.branch);
-    expect(sourceAction.OutputArtifacts.length).toEqual(1);
-    expect(sourceAction.OutputArtifacts[0].Name).toEqual(`source-${sourceDef.repo}`);
+    for (let i = 0; i < sourceDefs.length; i++) {
+        const sourceAction = sourceStage.Actions[i];
+        const def = sourceDefs[i];
+        expect(sourceAction.ActionTypeId.Category).toEqual('Source');
+        expect(sourceAction.ActionTypeId.Provider).toEqual('CodeStarSourceConnection');
+        expect(sourceAction.Configuration.ConnectionArn).toEqual(def.codeStarConnection);
+        expect(sourceAction.Configuration.FullRepositoryId).toEqual(`${def.repoOwner}/${def.repo}`);
+        expect(sourceAction.Configuration.BranchName).toEqual(def.branch);
+        expect(sourceAction.OutputArtifacts.length).toEqual(1);
+        expect(sourceAction.OutputArtifacts[0].Name).toEqual(`source-${def.repo}`);
+    }
 }
 
 const checkBuildStageValues = (buildStage: any, sourceDef: SourceDef, projectRef: string) => {
