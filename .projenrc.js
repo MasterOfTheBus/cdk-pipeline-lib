d4f6ed6d9519b8228e1e5371eda92690c889b2fb
Sidney Ng <yendisng@gmail.com>
2021-12-09 21:01:08 -0500

    define npm registry info; delete the package-lock file

--- .projenrc.js
+++ .projenrc.js
@@ -10,7 +10,10 @@ const project = new awscdk.AwsCdkConstructLibrary({
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  packageName: '@masterofthebus/cdk-pipeline-lib',  /* The "name" in package.json. */
   // release: undefined,      /* Add release management to this project. */
+  npmRegistryUrl: 'https://npm.pkg.github.com',
+  npmTokenSecret: 'GITHUB_TOKEN',
+  releaseToNpm: true
 });
 project.synth();
