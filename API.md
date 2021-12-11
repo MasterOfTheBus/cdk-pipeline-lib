1cb1be0f2da4b00feb6c6ca88376c0a9535e8770
Sidney Ng <yendisng@gmail.com>
2021-12-10 20:12:05 -0500

    chore: project created with projen (#16)
    * chore: project created with projen
    
    * define npm registry info; delete the package-lock file
    
    * removing both the jest config and ts config
    
    * set a min node version
    
    * comment out the git hooks and delete the main.yml github workflow

---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="classes"></a>
+
+### Hello <a name="@masterofthebus/cdk-pipeline-lib.Hello" id="masterofthebuscdkpipelinelibhello"></a>
+
+#### Initializers <a name="@masterofthebus/cdk-pipeline-lib.Hello.Initializer" id="masterofthebuscdkpipelinelibhelloinitializer"></a>
+
+```typescript
+import { Hello } from '@masterofthebus/cdk-pipeline-lib'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| [`sayHello`](#masterofthebuscdkpipelinelibhellosayhello) | *No description.* |
+
+---
+
+##### `sayHello` <a name="@masterofthebus/cdk-pipeline-lib.Hello.sayHello" id="masterofthebuscdkpipelinelibhellosayhello"></a>
+
+```typescript
+public sayHello()
+```
+
+
+
+
+
--- test/code-build-project.test.ts
+++ test/code-build-project.test.ts
@@ -1,55 +1,55 @@
 import { Capture, Template } from 'aws-cdk-lib/assertions';
-import * as cdk from 'aws-cdk-lib/core';
 import { Artifact } from 'aws-cdk-lib/aws-codepipeline';
-import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import * as cdk from 'aws-cdk-lib/core';
+import { CodeBuildProjectConstruct } from '../lib/code-build-project';
 import { CodeStarConnectionDef } from '../lib/source-def';
-import { CodeBuildProjectConstruct } from '../lib/code-build-project'
 
 test('Test Code Build Construct', () => {
-    const repo = "test-repo";
-    const repoOwner = "test-owner";
+  const repo = 'test-repo';
+  const repoOwner = 'test-owner';
 
-    const app = new cdk.App();
-    const stack = new cdk.Stack(app, 'TestStack');
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, 'TestStack');
 
-    const source = new CodeStarConnectionDef({
-        codeStarConnection: "arn:aws:codestar-connections:us-east-1:000000000000:connection/11111111-2222-3333-4444-555555555555",
-        repo: repo,
-        repoOwner: repoOwner
-    });
+  const source = new CodeStarConnectionDef({
+    codeStarConnection: 'arn:aws:codestar-connections:us-east-1:000000000000:connection/11111111-2222-3333-4444-555555555555',
+    repo: repo,
+    repoOwner: repoOwner,
+  });
 
-    const artifact = new Artifact();
-    const bucket = new Bucket(stack, "Bucket");
+  const artifact = new Artifact();
+  const bucket = new Bucket(stack, 'Bucket');
 
-    new CodeBuildProjectConstruct(stack, "CodeBuild", {
-        sourceArtifact: artifact,
-        sourceInfo: source,
-        deployBucket: bucket
-    });
+  new CodeBuildProjectConstruct(stack, 'CodeBuild', {
+    sourceArtifact: artifact,
+    sourceInfo: source,
+    deployBucket: bucket,
+  });
 
-    const template = Template.fromStack(stack);
+  const template = Template.fromStack(stack);
 
-    const projectCapture = new Capture();
+  const projectCapture = new Capture();
 
-    template.resourceCountIs("AWS::CodeBuild::Project", 1);
-    template.hasResource("AWS::CodeBuild::Project", projectCapture);
+  template.resourceCountIs('AWS::CodeBuild::Project', 1);
+  template.hasResource('AWS::CodeBuild::Project', projectCapture);
 
-    const project = projectCapture.asObject();
+  const project = projectCapture.asObject();
 
-    expect(project.Properties.Source).toBeTruthy();
-    expect(project.Properties.Source.Type).toEqual('GITHUB');
-    expect(project.Properties.Source.Location).toEqual(`https://github.com/${repoOwner}/${repo}.git`);
+  expect(project.Properties.Source).toBeTruthy();
+  expect(project.Properties.Source.Type).toEqual('GITHUB');
+  expect(project.Properties.Source.Location).toEqual(`https://github.com/${repoOwner}/${repo}.git`);
 
-    expect(project.Properties.Artifacts).toBeTruthy();
-    expect(project.Properties.Artifacts.Type).toEqual('S3');
-    expect(project.Properties.Artifacts.Packaging).toEqual('ZIP');
-    expect(project.Properties.Artifacts.Location).toBeTruthy();
+  expect(project.Properties.Artifacts).toBeTruthy();
+  expect(project.Properties.Artifacts.Type).toEqual('S3');
+  expect(project.Properties.Artifacts.Packaging).toEqual('ZIP');
+  expect(project.Properties.Artifacts.Location).toBeTruthy();
 
-    const expectedTriggerFilter = [
-        [
-            { Pattern: 'PUSH', Type: 'EVENT' },
-            { Pattern: 'refs/heads/main', Type: 'HEAD_REF' }
-        ]
-    ];
-    expect(project.Properties.Triggers.FilterGroups).toStrictEqual(expectedTriggerFilter);
+  const expectedTriggerFilter = [
+    [
+      { Pattern: 'PUSH', Type: 'EVENT' },
+      { Pattern: 'refs/heads/main', Type: 'HEAD_REF' },
+    ],
+  ];
+  expect(project.Properties.Triggers.FilterGroups).toStrictEqual(expectedTriggerFilter);
 });
