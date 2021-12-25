563f20e1cc55aabcbf1113ebc7c7b6169ee67cb3
Sidney Ng <yendisng@gmail.com>
2021-12-24 21:19:35 -0500

    exporting the new construct

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { MultiSourcePipeline } from './pipeline';
 import { CodeStarConnectionDef } from './source-def';
+import { CodePipelineConstruct } from './code-pipeline';
 
 export { CodeStarConnectionDef };
 export { MultiSourcePipeline };
+export { CodePipelineConstruct };
