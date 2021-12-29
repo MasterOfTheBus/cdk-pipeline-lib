c731d53bc38533c3f6ebc46fa21048ee43583239
Sidney Ng <yendisng@gmail.com>
2021-12-28 20:39:26 -0500

    adding test for pipeline construct

--- test/code-pipeline.test.ts
+++ test/code-pipeline.test.ts
@@ -1,33 +1,80 @@
 import { Template } from 'aws-cdk-lib/assertions';
+import { Artifact } from 'aws-cdk-lib/aws-codepipeline';
+import { Function, Runtime, S3Code } from 'aws-cdk-lib/aws-lambda';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import * as cdk from 'aws-cdk-lib/core';
+
+import { Construct } from 'constructs';
 import { CodePipelineConstruct } from '../src/code-pipeline';
 import { CodeStarConnectionDef } from '../src/source-def';
 
-test('Test Code Build Construct', () => {
-  const repoOwner = 'test-owner';
+// === Setup the classes for helping with testing ===
 
+class MyLambdaStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, bucketArn: string, key: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucket = Bucket.fromBucketArn(this, 'artifactBucket', bucketArn);
+
+    new Function(this, 'LambdaFunction', {
+      runtime: Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      code: new S3Code(bucket, key),
+    });
+  }
+}
+
+class MyPipelineAppStage extends cdk.Stage {
+
+  constructor(scope: Construct, id: string, bucketArn: string, key: string, props?: cdk.StageProps) {
+    super(scope, id, props);
+
+    new MyLambdaStack(this, 'LambdaStack', bucketArn, key);
+  }
+}
+
+class MyPipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucketArn = 'arn:aws:s3:::sng-test-bucket';
+
+    const pipelineSource = new CodeStarConnectionDef({
+      codeStarConnection: 'arn:aws:codestar-connections:us-east-1:000000000000:connection/11111111-2222-3333-4444-555555555555',
+      repo: 'pipeline-repo',
+      repoOwner: 'test-owner',
+    });
+
+    const source = new CodeStarConnectionDef({
+      codeStarConnection: 'arn:aws:codestar-connections:us-east-1:000000000000:connection/11111111-2222-3333-4444-555555555555',
+      repo: 'source-repo',
+      repoOwner: 'test-owner',
+    });
+
+    const sourceOutputArtifact = new Artifact();
+    const pipelineConstruct = new CodePipelineConstruct(this, 'TestPipeline', {
+      pipelineSource: pipelineSource,
+      source: source,
+      artifactBucketArn: bucketArn,
+      outputArtifact: sourceOutputArtifact,
+    });
+
+    // The output artifact key?
+    pipelineConstruct.pipeline.addStage(
+      new MyPipelineAppStage(this, 'test', bucketArn, sourceOutputArtifact.objectKey, {
+        env: { account: '025257542471', region: 'us-east-1' },
+      }),
+    );
+  }
+}
+
+// === The Actual Tests ===
+
+test('Test Code Pipeline Construct', () => {
   const app = new cdk.App();
-  const stack = new cdk.Stack(app, 'TestStack');
 
-  const artifactBucket = new Bucket(stack, 'ArtifactBucket');
-
-  const pipelineSource = new CodeStarConnectionDef({
-    codeStarConnection: 'arn:aws:codestar-connections:us-east-1:000000000000:connection/11111111-2222-3333-4444-555555555555',
-    repo: 'pipeline-repo',
-    repoOwner: repoOwner,
-  });
-
-  const source = new CodeStarConnectionDef({
-    codeStarConnection: 'arn:aws:codestar-connections:us-east-1:000000000000:connection/11111111-2222-3333-4444-555555555555',
-    repo: 'source-repo',
-    repoOwner: repoOwner,
-  });
-
-  new CodePipelineConstruct(stack, 'TestPipeline', {
-    pipelineSource: pipelineSource,
-    source: source,
-    artifactBucket: artifactBucket,
+  const stack = new MyPipelineStack(app, 'TestPipelineStack', {
+    env: { account: '025257542471', region: 'us-east-1' },
   });
 
   const template = Template.fromStack(stack);
@@ -36,7 +83,7 @@ test('Test Code Build Construct', () => {
   const projectResources = template.findResources('AWS::CodeBuild::Project');
 
   const sourceExpected = {
-    Location: `https://github.com/test-owner/${source.repo}.git`,
+    Location: 'https://github.com/test-owner/source-repo.git',
     ReportBuildStatus: true,
     Type: 'GITHUB',
   };
