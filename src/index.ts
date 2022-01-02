278acaf532f337302987f29d4a90524fdaf2b69e
Sidney Ng <yendisng@gmail.com>
2022-01-02 11:02:26 -0500

    removing MultiSourcePipeline from the API

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { CodePipelineConstruct } from './code-pipeline';
-import { MultiSourcePipeline } from './pipeline';
 import { CodeStarConnectionDef } from './source-def';
 
 export { CodeStarConnectionDef };
-export { MultiSourcePipeline };
 export { CodePipelineConstruct };
